3c2757bf6275eef96401b16beee5628fc403bd6a
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2026-04-19 19:03:08 +0530

    feat: graph builder and node definition

---
+++ src/graph_builder/graph_builder.py
@@ -0,0 +1,30 @@
+from langgraph.graph import StateGraph, END
+from src.state.rag_state import RAGState
+from src.nodes.nodes import RAGNodes
+
+
+class GraphBuilder:
+    def __init__(self, retriever, llm):
+        self.nodes = RAGNodes()
+        self.graph = None
+
+    def build(self):
+        builder = StateGraph(RAGState)
+
+        builder.add_node("retriever", self.nodes.retrieve_docs)
+        builder.add_node("responder", self.nodes.generate_answer)
+
+        builder.set_entry_point("retriever")
+
+        builder.add_edge("retriever", "responder")
+        builder.add_edge("responder", END)
+
+        self.graph = builder.compile()
+        return self.graph
+    
+    def run(self, question: str)->dict:
+        if self.graph is None:
+            self.build()
+        
+        initial_state = RAGState(question=question)
+        return self.graph.invoke(initial_state)
